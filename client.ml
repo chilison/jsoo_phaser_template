2731eeb12770c44518464c43dec8aca47f1139f1
Kakadu <Kakadu@pm.me>
2022-10-25 18:24:34 +0300

    CreateSceneFromObje3ctConfig is rewriteen as ocaml object
    With a little bit of unsafe trickery
    
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- client.ml
+++ client.ml
@@ -4,22 +4,6 @@
 
 open Js_of_ocaml
 open Firebug
-open Js_of_ocaml.WebSockets
-
-let () = assert (WebSockets.is_supported ())
-
-let () =
-  let sock = new%js webSocket (Js.string "ws://localhost:3000") in
-  sock##.onopen :=
-    Dom.handler (fun _e ->
-        console##log (Js.string "opened");
-        sock##send (Js.string "ping");
-        Js._true);
-
-  sock##.onmessage :=
-    Dom.handler (fun e ->
-        console##log_2 (Js.string "onmessage:") e##.data;
-        Js._true)
 
 (* Dirty hack. OCaml library for phaser should be constructed and used type safely.
    Below there is an attempt to rewrite it in more type-safe OCaml-like way.
@@ -57,18 +41,81 @@ let __ () =
 let scene =
   Js.Unsafe.eval_string
     {|
+    // https://photonstorm.github.io/phaser3-docs/Phaser.Types.Scenes.html#.CreateSceneFromObjectConfig
     const scene = {
+      init: function () {
+        console.log("scene.init", "this = ", this );
+      },
       preload: function () {
-        //console.log("scene.preload");
+        console.log("scene.preload", "this = ", this );
+        console.log("this.load = ", this.load );
+
         this.load.bitmapFont("arcade", "assets/font/arcade.png", "assets/font/arcade.xml");
       },
       create: function () {
-        //console.log("scene.create");
+        console.log("scene.create", "this = ", this );
         this.add.bitmapText(400, 300, "arcade", "Hello Phaser").setOrigin(0.5);
       }
     };
     scene; // last value is a result of expression
 |}
+
+open Js
+
+(** See also: https://photonstorm.github.io/phaser3-docs/Phaser.Loader.LoaderPlugin.html *)
+class type loader_plugin =
+  object
+    method bitmapFont : js_string t -> js_string t -> js_string t -> unit meth
+  end
+
+(* TODO: understand what an object it is, and which class name is desirable *)
+class type o =
+  object
+    method setOrigin : float -> unit meth
+  end
+
+(* TODO: understand what an object it is, and which class name is desirable *)
+class type xxxx =
+  object
+    method bitmapText : int -> int -> js_string t -> js_string t -> o t meth
+  end
+
+(* It looks like we are constructing this object here:
+   https://photonstorm.github.io/phaser3-docs/Phaser.Types.Scenes.html#.CreateSceneFromObjectConfig__anchor *)
+let caml_scene =
+  (* TODO: in the line below it gives a warning about unused variable 'this'. No idea why, it shouldn't *)
+  object%js (this)
+    method init = ()
+
+    method preload =
+      (* In two functions below Javascript expects (I think) that a BIG object will be extended
+         by fields of small one (which is below). It seems to be a common pattern in JS worls,
+         but not in the statically typed one. There is high change that unsafe hacks will be
+         really needed. But it's worth investigating *)
+      Firebug.console##log (Js.string "preload");
+      console##log_2 (Js.string "self = ") this;
+      let loader : loader_plugin t =
+        (Js.Unsafe.eval_string {|x => x.load |} : _ -> _) this
+      in
+      console##log_2 (Js.string "laod =  ") loader;
+      loader##bitmapFont (Js.string "arcade")
+        (Js.string "assets/font/arcade.png")
+        (Js.string "assets/font/arcade.xml")
+
+    method create () =
+      Firebug.console##log (Js.string "Create");
+      console##log_2 (Js.string "self = ") this;
+      let add : xxxx t =
+        (Js.Unsafe.eval_string {|x => x.add |} : _ -> _) this
+      in
+      console##log_2 (Js.string "add =  ") add;
+      let (o : o t) =
+        add##bitmapText 400 300 (Js.string "arcade") (Js.string "Hello Phaser")
+      in
+      o##setOrigin 0.5
+
+    (* Long term recommendation is to design OCaml API in a manner that game developer will not need to do any unsafe hacks *)
+  end
 
 type game
 
@@ -83,7 +130,7 @@ let config =
     val backgroundColor = Js.string "#000"
     val parent = Js.string "game"
     val pixelArt = Js._true
-    val scene = scene
+    val scene = caml_scene
 
     val physics =
       object%js
@@ -99,5 +146,4 @@ let config =
       end
   end
 
-(* let () = Firebug.console##log config *)
 let _game = new%js make_game config
